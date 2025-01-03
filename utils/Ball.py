8a6b3284de2c73b9b22afd3a9f323c3229061732
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2025-01-03 15:57:21 +0530

    Update to use utils.constant and Add increase_speed method

--- utils/Ball.py
+++ utils/Ball.py
@@ -1,25 +1,33 @@
 import cv2
 
+from utils.constants import SPEED_INCREMENT,BALL_RADIUS,BALL_VEL,BALL_COLOR
+
 class Ball:
-    def __init__(self, x, y, radius, MAX_VEL, ball_color):
+    def __init__(self, x, y):
         self.x = self.original_x = x
         self.y = self.original_y = y
-        self.radius = radius
-        self.MAX_VEL = MAX_VEL
-        self.ball_color = ball_color
-        self.x_vel = MAX_VEL
-        self.y_vel = -1*MAX_VEL
+        self.radius = BALL_RADIUS
+        self.vel = BALL_VEL
+        self.color = BALL_COLOR
+        self.x_vel = BALL_VEL
+        self.y_vel = -1*BALL_VEL
 
-    def draw(self, canvas):
-        cv2.circle(canvas, (int(self.x), int(self.y)),
-                   self.radius, (255, 255, 255), -1)
+    def draw(self, frame):
+        cv2.circle(frame, (int(self.x), int(self.y)),
+                   self.radius,self.color, -1)
 
-    def move(self):
+    def move(self,frame):
         self.x += self.x_vel
         self.y += self.y_vel
+
+        self.draw(frame)
+    
+    def increase_speed(self):
+        self.x_vel *= SPEED_INCREMENT
+        self.y_vel *= SPEED_INCREMENT
 
     def reset(self):
         self.x = self.original_x
         self.y = self.original_y
-        self.x_vel = self.MAX_VEL
-        self.y_vel = -1*self.MAX_VEL
+        self.x_vel = self.vel
+        self.y_vel = -1*self.vel
